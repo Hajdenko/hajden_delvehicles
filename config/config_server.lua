d71df061462c54acdb200498832809cd7438c58e
Hajden <138399356+Hajdenko@users.noreply.github.com>
2024-10-10 13:12:16 +0200

    Update config_server.lua

---
+++ config/config_server.lua
@@ -0,0 +1,6 @@
+sv_config = {
+    getGroup = function(source)
+        local xPlayer = exports["es_extended"]:getSharedObject().GetPlayerFromId(source)
+        return xPlayer.getGroup()
+    end
+}
